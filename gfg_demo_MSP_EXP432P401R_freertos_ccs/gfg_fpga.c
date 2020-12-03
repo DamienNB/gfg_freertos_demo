97939ba349c1d04a30af00f4865ec3d2ff7f12ac
Damien Nikola Bobrek <dbobrek@ufl.edu>
2020-12-03 18:18:02 -0500

    Added basic and common transactions to gfg_fpga driver

--- gfg_demo_MSP_EXP432P401R_freertos_ccs/gfg_fpga.c
+++ gfg_demo_MSP_EXP432P401R_freertos_ccs/gfg_fpga.c
@@ -9,26 +9,87 @@
 
 static SPI_Handle spi_handle = NULL;
 
-int_fast8_t gfg_fpga_write_reg(uint8_t reg_addr, uint32_t reg_val) {
-    int_fast8_t ierror = 0;
+int_fast8_t gfg_fgpa_init(SPI_Handle spiHndl) {
+    spi_handle = spiHndl;
 
-    uint8_t tx_buf[5];
-
-    // load the command and register address
-    tx_buf[0] = 0x40 | (reg_addr & 0x1F);
-
-    // swap the endianess of reg_val and load it into tx_buf
-    int_fast8_t i;
-    for(i = 1; i <= 4; i++) {
-        tx_buf[i] = *(((uint8_t *) &reg_val) + (4-i));
+    // See if a SPI works by trying to send a series of NO_OP commands
+    if(!gfg_fpga_send_5_no_ops())
+    {
+        return -1;
     }
 
-    uint8_t rx_buf[5] = {0};
+    // Check to see if a register can be written to
+    if(!gfg_fpga_write_reg(GFG_FPGA_TRIANGLE_POINT_0_X_ADDR, 37))
+    {
+        return -2;
+    }
+    if(gfg_fpga_read_reg(GFG_FPGA_TRIANGLE_POINT_0_X_ADDR) != 37)
+    {
+        return -3;
+    }
+
+    // Check to see if a register can be re-written to
+    if(!gfg_fpga_write_reg(GFG_FPGA_TRIANGLE_POINT_0_X_ADDR, 0))
+    {
+        return -4;
+    }
+    if(gfg_fpga_read_reg(GFG_FPGA_TRIANGLE_POINT_0_X_ADDR) != 0)
+    {
+        return -5;
+    }
+
+    // Try to clear the screen
+    if(!gfg_fpga_write_triangle(0, 0, 0, 0, 0, 0, 0))
+    {
+        return -6;
+    }
+
+    return 0;
+}
+
+
+int_fast64_t gfg_fpga_read_reg(uint8_t reg_addr) {
+    // Two read commands are sent in succession.
+    // The correct value is returned on the second read, but depends on the
+    // address requested by the first read.
+
+    uint8_t tx_buf[10] = { 0xFF };
+
+    // load the read command and register address twice
+    tx_buf[0] = GFG_FPGA_READ_CMD | (reg_addr & 0x1F);
+    tx_buf[5] = GFG_FPGA_READ_CMD | (reg_addr & 0x1F);
+
+    uint8_t rx_buf[10] = { 0x00 };
 
     SPI_Transaction spiTransaction;
     spiTransaction.txBuf = tx_buf;
-    spiTransaction.count = 5;
+    spiTransaction.count = sizeof(tx_buf);
     spiTransaction.rxBuf = rx_buf;
+
+    if(!SPI_transfer(spi_handle, &spiTransaction))
+    {
+        return -1;
+    }
+
+    uint32_t value_read = 0;
+    value_read |= ((uint32_t)tx_buf[6]) << 24u;
+    value_read |= ((uint32_t)tx_buf[7]) << 16u;
+    value_read |= ((uint32_t)tx_buf[8]) <<  8u;
+    value_read |= ((uint32_t)tx_buf[9]) <<  0u;
+
+    return value_read;
+}
+
+int_fast8_t gfg_fpga_send_5_no_ops(void) {
+    int_fast8_t ierror = 0;
+
+    // See if a SPI works by trying to send a series of NO_OP commands
+    uint8_t tx_buf[5] = { GFG_FPGA_NO_OP_CMD };
+
+    SPI_Transaction spiTransaction;
+    spiTransaction.txBuf = tx_buf;
+    spiTransaction.count = sizeof(tx_buf);
+    spiTransaction.rxBuf = NULL; // discard data received
 
     if(!SPI_transfer(spi_handle, &spiTransaction))
     {
@@ -38,6 +99,95 @@ int_fast8_t gfg_fpga_write_reg(uint8_t reg_addr, uint32_t reg_val) {
     return ierror;
 }
 
-int_fast8_t gfg_fgpa_init(SPI_Handle spiHndl) {
-    spi_handle = spiHndl;
+int_fast8_t gfg_fpga_write_reg(uint8_t reg_addr, uint32_t reg_val) {
+    int_fast8_t ierror = 0;
+
+    uint8_t tx_buf[5];
+
+    // load the command and register address
+    tx_buf[0] = GFG_FPGA_WRITE_CMD | (reg_addr & 0x1F);
+    // load the value into the buffer to be transmitted in big endian order.
+    tx_buf[1] = (uint8_t) ((reg_val & 0xff000000) >> 24u);
+    tx_buf[2] = (uint8_t) ((reg_val & 0x00ff0000) >> 16u);
+    tx_buf[3] = (uint8_t) ((reg_val & 0x0000ff00) >>  8u);
+    tx_buf[4] = (uint8_t) ((reg_val & 0x000000ff) >>  0u);
+
+    SPI_Transaction spiTransaction;
+    spiTransaction.txBuf = tx_buf;
+    spiTransaction.count = sizeof(tx_buf);
+    spiTransaction.rxBuf = NULL; // discard data received
+
+    if(!SPI_transfer(spi_handle, &spiTransaction))
+    {
+        ierror = -1;
+    }
+
+    return ierror;
+}
+
+int_fast8_t gfg_fpga_write_triangle(uint_fast16_t point_0_x,
+                                    uint_fast16_t point_0_y,
+                                    uint_fast16_t point_1_x,
+                                    uint_fast16_t point_1_y,
+                                    uint_fast16_t point_2_x,
+                                    uint_fast16_t point_2_y,
+                                    uint_fast16_t color) {
+    int_fast8_t ierror = 0;
+
+    uint8_t tx_buf[] =
+    {
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_POINT_0_X_ADDR,
+        0,
+        0,
+        (point_0_x & 0xff00) >> 8u,
+        (point_0_x & 0x00ff),
+
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_POINT_0_Y_ADDR,
+        0,
+        0,
+        (point_0_y & 0xff00) >> 8u,
+        (point_0_y & 0x00ff),
+
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_POINT_1_X_ADDR,
+        0,
+        0,
+        (point_1_x & 0xff00) >> 8u,
+        (point_1_x & 0x00ff),
+
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_POINT_1_Y_ADDR,
+        0,
+        0,
+        (point_1_y & 0xff00) >> 8u,
+        (point_1_y & 0x00ff),
+
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_POINT_2_X_ADDR,
+        0,
+        0,
+        (point_2_x & 0xff00) >> 8u,
+        (point_2_x & 0x00ff),
+
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_POINT_2_Y_ADDR,
+        0,
+        0,
+        (point_2_y & 0xff00) >> 8u,
+        (point_2_y & 0x00ff),
+
+        GFG_FPGA_WRITE_CMD | GFG_FPGA_TRIANGLE_COLOR_ADDR,
+        0,
+        0,
+        (color & 0x0f00) >> 8u,
+        (color & 0x00ff)
+    };
+
+    SPI_Transaction spiTransaction;
+    spiTransaction.txBuf = tx_buf;
+    spiTransaction.count = sizeof(tx_buf);
+    spiTransaction.rxBuf = NULL; // discard data received
+
+    if(!SPI_transfer(spi_handle, &spiTransaction))
+    {
+        ierror = -1;
+    }
+
+    return ierror;
 }
--- gfg_demo_MSP_EXP432P401R_freertos_ccs/gfg_fpga.h
+++ gfg_demo_MSP_EXP432P401R_freertos_ccs/gfg_fpga.h
@@ -11,8 +11,38 @@
 #include <stdint.h>
 #include <ti/drivers/SPI.h>
 
-int_fast8_t gfg_fpga_write_reg(uint8_t reg_addr, uint32_t reg_val);
+/*******************/
+/**\name GFG_FPGA COMMANDS */
+/*******************/
+#define GFG_FPGA_NO_OP_CMD                      (0x00)
+#define GFG_FPGA_WRITE_CMD                      (0x80)
+#define GFG_FPGA_READ_CMD                       (0x40)
+
+/*******************/
+/**\name GFG_FPGA REGISTERS */
+/*******************/
+#define GFG_FPGA_TRIANGLE_POINT_0_X_ADDR        (0x00)
+#define GFG_FPGA_TRIANGLE_POINT_0_Y_ADDR        (0x01)
+#define GFG_FPGA_TRIANGLE_POINT_1_X_ADDR        (0x02)
+#define GFG_FPGA_TRIANGLE_POINT_1_Y_ADDR        (0x03)
+#define GFG_FPGA_TRIANGLE_POINT_2_X_ADDR        (0x04)
+#define GFG_FPGA_TRIANGLE_POINT_2_Y_ADDR        (0x05)
+#define GFG_FPGA_TRIANGLE_COLOR_ADDR            (0X06)
 
 int_fast8_t gfg_fgpa_init(SPI_Handle spiHndl);
+
+int_fast64_t gfg_fpga_read_reg(uint8_t reg_addr);
+
+int_fast8_t gfg_fpga_send_5_no_ops(void);
+
+int_fast8_t gfg_fpga_write_reg(uint8_t reg_addr, uint32_t reg_val);
+
+int_fast8_t gfg_fpga_write_triangle(uint_fast16_t point_0_x,
+                                    uint_fast16_t point_0_y,
+                                    uint_fast16_t point_1_x,
+                                    uint_fast16_t point_1_y,
+                                    uint_fast16_t point_2_x,
+                                    uint_fast16_t point_2_y,
+                                    uint_fast16_t color);
 
 #endif /* GFG_FPGA_H_ */
